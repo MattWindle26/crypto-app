aacc2f1523b27a5563a97ddde576b8edc7c9e069
EKMmattwindle <matthewwindle@ekm.com>
2022-12-05 12:18:44 +0000

    Added some grid changes
    Added some grid changes

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,9 @@ export default function Document() {
         {/* https://www.cryptofonts.com/index.html#get-started */}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/monzanifabio/cryptofont/cryptofont.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"  />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
